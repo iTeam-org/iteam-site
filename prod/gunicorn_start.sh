ef00629c4a94e217774d99bbf200323d2807a268
Adrien Chardon <adrienchardon@mailoo.org>
2014-10-26 14:13:52 +0100

    few fix

--- prod/gunicorn_start.sh
+++ prod/gunicorn_start.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status.
 
-NAME="iTeam"
 WORKINGDIR=/home/nodraak/Telechargements/iteam.org/
 
 LOGFILE=$WORKINGDIR/gunicorn.log
 ERRFILE=$WORKINGDIR/gunicorn_err.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3 # how many worker processes : should be nb_cpu*2+1
-# SOCKFILE=/home/nodraak/Telechargements/iteam.org/gunicorn.sock
 USER=nodraak
 GROUP=nodraak
 
-echo "Starting gunicorn server for $NAME"
-
 cd $WORKINGDIR
-exec ../bin/gunicorn wsgi:application \
+exec ../bin/gunicorn iTeam.wsgi:application \
 -b localhost:8000 \
-#--bind=unix:$SOCKFILE
 --timeout=300 \
 --workers $NUM_WORKERS \
 --name $NAME \
